a06718075d1ac66fda3adf75d432cacee4a6ed1e
Ishraq Ahmed <ishraqahmedbd@gmail.com>
2021-12-04 22:19:56 +0600

    initial update l5 t2

---
+++ Lab 5/task2.asm
@@ -0,0 +1,57 @@
+
+.MODEL SMALL  
+
+.STACK 100H  
+
+.DATA
+    ; DEFINE YOUR VARIABLES HERE
+    a db 0
+    b db 2
+    c db ?
+    sum db ?
+    count db 10
+    i db 0
+
+.CODE
+    MAIN PROC
+        
+        MOV AX, @DATA
+        MOV DS, AX
+        
+        ; YOUR CODE STARTS HERE
+        mov cl, count  ;count->cl
+        hack:
+            cmp i, cl
+            jg result
+            mov al, a
+            sub al, 30h
+            mov bl, b
+            sub bl, 30h
+            mov dl, c
+            add al, bl  ;al=al+bl(a+b)
+            mov bh, al  ;bh->al
+            jmp calc
+            
+        calc:
+            add sum, bh
+            mov al, bl  ;a=b
+            mov bl, dl  ;b=c
+            inc i
+            jmp hack
+            
+        result:
+            mov dl, sum
+            add dl, 30h
+            mov ah, 2
+            int 21h  
+             
+         
+        
+        ; YOUR CODE ENDS HERE
+        
+        MOV AX, 4C00H
+        INT 21H
+        
+    MAIN ENDP
+   
+    END MAIN
