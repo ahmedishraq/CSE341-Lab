6ed88ee635636be7ee5b22b8c33251eb25180294
Ishraq Ahmed <ishraqahmedbd@gmail.com>
2021-12-08 17:46:41 +0600

    final update l5 t3

--- Lab 5/task3.asm
+++ Lab 5/task3.asm
@@ -6,9 +6,9 @@
 .DATA
     ; DEFINE YOUR VARIABLES HERE
     
-    tmp dw ?
+    ;tmp dw ?
     sum dw 0 
-    subtrct dw ?
+    ;subtrct dw ?
 
 .CODE
     MAIN PROC
@@ -30,27 +30,50 @@
         int 21h
         mov ah, 0h
         sub ax, 30h
+        mov cx, ax
         
-        mov tmp, ax
+        ;mov tmp, ax
         ;mov cx, ax
-        mov cx, 0h
+        ;mov cx, 0h
         
         ;choose option for function
+        mov ah, 1
+        int 21h 
+        mov ah, 0h
+        
+        check:
+            cmp ax, 31h
+            je div_value
+            cmp ax, 32h
+            je mul_value
+            
+        ;value transfer for mul
+        mul_value:
+            mov ax, cx
+            mov cx, 0h
+            jmp multp
+            
+        ;value transfer for div
+        div_value:
+            mov ax, cx
+            mov cx, 0h
+            jmp divd
         
         
         ;multiplication method
-        ;multp:
-         ;   cmp cx, bx
-          ;  jge multp_rst
-           ; add sum, ax
-            ;inc cx
-            ;jmp multp
+        multp:
+            cmp cx, bx
+            jge multp_rst
+            add sum, ax
+            inc cx
+            jmp multp
             
-        ;multp_rst:
-         ; add sum, 30h
-          ;mov dx, sum
-          ;mov ah, 2
-          ;int 21h
+        multp_rst:
+          add sum, 30h
+          mov dx, sum
+          mov ah, 2
+          int 21h
+          jmp exit
           
         ;division method
         divd:
@@ -66,6 +89,11 @@
             mov dx, bx
             mov ah, 2
             int 21h
+            jmp exit
+            
+        exit:
+        mov ax, 4c00h
+        int 21h
             
                
             
