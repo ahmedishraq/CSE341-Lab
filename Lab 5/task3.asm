a0bd67c8436f93c548cb2fc8c6ece55ba6a8e99b
Ishraq Ahmed <ishraqahmedbd@gmail.com>
2021-12-08 16:18:37 +0600

    update l5 t3

--- Lab 5/task3.asm
+++ Lab 5/task3.asm
@@ -8,7 +8,7 @@
     
     tmp dw ?
     sum dw 0 
-    subtrct ?
+    subtrct dw ?
 
 .CODE
     MAIN PROC
@@ -17,6 +17,38 @@
         MOV DS, AX
         
         ; YOUR CODE STARTS HERE
+        
+        ;1st input
+        mov ah, 1
+        int 21h
+        mov ah, 0h
+        mov bx, ax
+        sub bx, 30h
+        
+        ;2nd input
+        mov ah, 1
+        int 21h
+        mov ah, 0h
+        sub ax, 30h
+        
+        mov tmp, ax
+        mov cx, ax
+        mov bx, 0h
+        
+        ;multiplication method
+        multp:
+            cmp bx, ax
+            jge multp_rst
+            add sum, ax
+            inc bx
+            jmp multp
+            
+        multp_rst:
+          add sum, 30h
+          mov dx, sum
+          mov ah, 2
+          int 21h   
+            
          
         
         ; YOUR CODE ENDS HERE
