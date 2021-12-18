2b3a58f3325e8ec25b7a83ae1113bfa7789c962a
Ishraq Ahmed <ishraqahmedbd@gmail.com>
2021-12-19 01:50:21 +0600

    final submit update l6 t2

--- Lab 6/task2.asm
+++ Lab 6/task2.asm
@@ -4,7 +4,6 @@
 .STACK 100H  
 
 .DATA
-    ; DEFINE YOUR VARIABLES HERE
     
     n db 4 dup(?)
     
@@ -19,8 +18,6 @@
         MOV AX, @DATA
         MOV DS, AX
         
-        ; YOUR CODE STARTS HERE
-        
         ;showing enter number msg
         lea dx, a
         mov ah, 9
@@ -33,21 +30,18 @@
         
         ;taking array input using loop
         run:
-            int 21h
-            ;sub al, 30h 
+            int 21h 
             mov n[si], al
             add si, 1
             loop run
             
         ;adding odd numbers from the array
-        ;mov cx, 4
-        mov bx, 1
-        mov si, 1
+        mov bx, 0
+        mov si, 0
         mov ah, 2
         
         
         ;checking odd index
-        
         check_odd:
             cmp bx, 4
             jl add_odd
@@ -56,7 +50,6 @@
         
         add_odd:
             mov dl, n[si]
-            ;add dl, 30h
             add sum, dl
             add si, 2
             add bx, 2 
@@ -78,29 +71,9 @@
             int 21h
         
             mov dl, sum
-            ;add dl, 30h
+            sub dl, 30h
             mov ah, 2
             int 21h
-            
-        
-        
-        
-        
-        ;printing the array elements
-       ; mov cx, 4
-;        mov ah, 2
-;        mov si, 0
-;        
-;        show:
-;            mov dl, n[si]
-;            add dl, 30h
-;            int 21h
-;            add si, 1
-;            loop show 
-            
-         
-        
-        ; YOUR CODE ENDS HERE
         
         MOV AX, 4C00H
         INT 21H
